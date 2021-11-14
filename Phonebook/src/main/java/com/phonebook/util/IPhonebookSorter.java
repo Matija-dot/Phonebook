37eec18ec2924abe708bdc3bfb9bb55ebff3cb73
Matija-dot <62620373+Matija-dot@users.noreply.github.com>
2021-11-14 21:23:24 +0100

    Delete Phonebook directory

--- Phonebook/src/main/java/com/phonebook/util/IPhonebookSorter.java
+++
@@ -1,9 +0,0 @@
-package com.phonebook.util;
-
-import com.phonebook.model.Contact;
-
-import javafx.collections.ObservableList;
-
-public interface IPhonebookSorter {
-    void sort(ObservableList<Contact> phonebook);
-}
